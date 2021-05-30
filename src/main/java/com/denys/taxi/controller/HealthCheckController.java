b7875c79a97181d3404342350ec124ccb52f94cc
Denys-Doskochynskiy <55959253+Denys-Doskochynskiy@users.noreply.github.com>
2021-05-30 20:31:54 +0300

    Update HealthCheckController.java

--- src/main/java/com/denys/taxi/controller/HealthCheckController.java
+++ src/main/java/com/denys/taxi/controller/HealthCheckController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class HealthController {
+public class HealthCheckController {
   
   @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
   public ResponseEntity returnHealthCheckResponse() {
